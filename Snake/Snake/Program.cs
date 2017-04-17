d19bf09c35e5b0c4ff8e49e61e0dd3749c322e46
Nikita <nikitagorinvg@gmail.com>
2017-04-17 13:36:24 +0300

    ctor

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,7 +11,14 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Point p1 = new Point(2, 3, '*');
+            p1.Draw();
+            Thread.Sleep(500);
+            Point p2 = new Point(5, 3, '#');
+
+            p2.Draw();
+
+
             Console.ReadKey();
         }
     }
