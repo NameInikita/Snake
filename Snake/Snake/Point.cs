86af497b9d7d0f7d467a32e3fa7af8bb99a36222
Nikita <nikitagorinvg@gmail.com>
2017-04-21 11:11:45 +0300

    working

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -22,12 +22,36 @@ namespace Snake
             this.y = y;
             this.sym = sym;
         }
-        
+
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+            this.sym = p.sym;
+        }
+
+        public void  Move(int offset,Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+                x = x + offset;
+            else if (direction == Direction.LEFT)
+                x = x - offset;
+            else if (direction == Direction.UP)
+                y = y + offset;
+            else if (direction==Direction.DOWN)
+                y = y - offset;
+
+        }
+
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+        public override string ToString()
+        {
+            return x + "," + y + "," + sym;
         }
 
     }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,16 +14,18 @@ namespace Snake
             Console.SetBufferSize(80, 25);
 
             HorizontalLines hl = new HorizontalLines(0,79,0,'+');
-            hl.Drow();
+            hl.DrawVLine();
             HorizontalLines hl1 = new HorizontalLines(0,79, 24, '+');
-            hl1.Drow();
+            hl1.DrawVLine();
             VerticalLine vl = new VerticalLine(0, 24, 0, '+');
             vl.DrawVLine();
             VerticalLine vl1 = new VerticalLine(0, 24, 78, '+');
             vl1.DrawVLine();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);//координаты, длина змейки, направление 
+            snake.DrawVLine();
+
 
 
             Console.ReadKey();
